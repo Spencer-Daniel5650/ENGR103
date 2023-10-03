1e064630f54228c091d4e41675eb5d651c452bd1
Spencer-Daniel5650 <danspencer5650@gmail.com>
2023-10-02 20:34:22 -0400

    animal.py

--- animal.py
+++ animal.py
@@ -4,22 +4,18 @@
 # Description: Tests different inputs and prints the results before asking for the user's favorite animal.
 
 # Test input: velociraptor
-print("Testing input: velociraptor")
 print("Your favorite animal is the velociraptor.")
 print()
 
 # Test input: tardigrade
-print("Testing input: tardigrade")
 print("Your favorite animal is the tardigrade.")
 print()
 
 # Test input: .
-print("Testing input: .")
 print("Your favorite animal is the .")
 print()
 
 # Test input: giant sloth
-print("Testing input: giant sloth")
 print("Your favorite animal is the giant sloth.")
 print()
 
