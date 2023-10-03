f8ae1e1ec199188aabc5d8d388ea2b9571947999
Spencer-Daniel5650 <danspencer5650@gmail.com>
2023-10-02 22:43:49 -0400

    animal.py

--- animal.py
+++ animal.py
@@ -1,6 +1,6 @@
 # Author: Daniel Spencer
 # Github Username: Spencer-Daniel5650
-# Date 10/01/2023
+# Date: 10/01/2023
 # Discription: Asks the user for their favorite animal and then prints out that that is their facorite animal
 
 fave_animal= input("Please enter your favorite animal: ")
