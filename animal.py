0585b2da5f2e8c4f3de74b18cac16195f9708ee4
Spencer-Daniel5650 <danspencer5650@gmail.com>
2023-10-02 21:08:25 -0400

    animal.py

--- animal.py
+++ animal.py
@@ -1,29 +1,7 @@
 # Author: Daniel Spencer
 # Github Username: Spencer-Daniel5650
 # Date 10/01/2023
-# Description: Tests different inputs and prints the results before asking for the user's favorite animal.
+# Discription: Asks the user for their favorite animal and then prints out that that is their facorite animal
 
-# Test input: velociraptor
-print("Your favorite animal is the velociraptor.")
-print()
-
-# Test input: tardigrade
-print("Your favorite animal is the tardigrade.")
-print()
-
-# Test input: .
-print("Your favorite animal is the .")
-print()
-
-# Test input: giant sloth
-print("Your favorite animal is the giant sloth.")
-print()
-
-fave_animal = input("What is your favorite animal? ")
-
-if fave_animal:  # Check if the input is not empty
-    print("Your favorite animal is the " + fave_animal + ".")
-else:
-    print("You did not enter a favorite animal.")
-
-
+fave_animal= input("Please enter your favorite animal: ")
+print( "your favorite animal is the " + fave_animal + ".")
