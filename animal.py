a565e708c486c87bd269c6fd2fb9ea8f78944ee1
Spencer-Daniel5650 <danspencer5650@gmail.com>
2023-10-03 12:14:12 -0400

    Merge remote-tracking branch 'origin/main'

--- animal.py
+++ animal.py
@@ -1,19 +1,19 @@
 # Author: Daniel Spencer
-# Github Username: Spencer-Daniel5650
-# Date 10/01/2023
+# GitHub Username: Spencer-Daniel5650
+# Date: 10/01/2023
 # Description: Tests different inputs and prints the results before asking for the user's favorite animal.
 
 # Test input: velociraptor
-input("Your favorite animal is the velociraptor.")
+fav_animal = "velociraptor"
 
 # Test input: tardigrade
-input("Your favorite animal is the tardigrade.")
+fav_animal = "tardigrade"
 
 # Test input: .
-input("Your favorite animal is the .")
+fav_animal = "."
 
 # Test input: giant sloth
-input("Your favorite animal is the giant sloth.")
+fav_animal = "giant sloth"
 
 fave_animal = input("What is your favorite animal? ")
 print("Your favorite animal is the " + fave_animal + ".")
