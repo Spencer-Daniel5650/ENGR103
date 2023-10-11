ee97536e94f312c1da2f7d86c35ac9805d9820f2
Spencer-Daniel5650 <danspencer5650@gmail.com>
2023-10-10 17:04:08 -0700

    Merge remote-tracking branch 'origin/main'

--- animal.py
+++
@@ -1,7 +0,0 @@
-# Author: Daniel Spencer
-# GitHub Username: Spencer-Daniel5650
-# Date: 10/01/2023
-# Discription: Asks the user for their favorite animal and then prints out that that is their favorite animal
-
-fave_animal = input("Please enter your favorite animal: ")
-print("Your favorite animal is the " + fave_animal + ".")
