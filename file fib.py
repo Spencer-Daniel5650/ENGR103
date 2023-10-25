897be6dcb62b13a0160df671e07715855d487af9
Spencer-Daniel5650 <danspencer5650@gmail.com>
2023-10-25 19:15:52 -0400

    New

--- file fib.py
+++ file fib.py
@@ -3,22 +3,19 @@
 # Date: 10/23/2023
 # Description: Creates a function named fib that takes positive integer and returns the numerical location it resides in the fibonacci sequence.
 
-def fib(n):
-    if n <= 0:
-        return 0
-
+def test_fib(n):
     fib_prev = 1
     fib_current = 1
 
-    for _ in range(3, n + 1):
-        fib_next = fib_prev + fib_current
-        fib_prev, fib_current = fib_current, fib_next
-
-    return fib_current
-
-# Call the fib function to calculate the nth Fibonacci number
-n = 10  # Replace with the desired position in the Fibonacci sequence
-term = fib(n)
+    if n == 1 or n == 2:
+        return 1
+    else:
+        for _ in range(3, n + 1):
+            fib_next = fib_prev + fib_current
+            fib_prev, fib_current = fib_current, fib_next
+        return fib_current
+n = 10
+test_fib(n)
 
 
 
