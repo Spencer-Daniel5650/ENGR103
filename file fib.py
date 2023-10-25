4ba6fff1a783883231a9fc587cf0f33cbc819038
Spencer-Daniel5650 <danspencer5650@gmail.com>
2023-10-25 18:42:11 -0400

    New

--- file fib.py
+++ file fib.py
@@ -3,25 +3,18 @@
 # Date: 10/23/2023
 # Description: Creates a function named fib that takes positive integer and returns the numerical location it resides in the fibonacci sequence.
 
-def hailstone(n):
-    if n == 1:
-        return 0
+def fib(n):
+    if n <= 2:
+        return 1
+    else:
+        a, b = 1, 1
+        for _ in range(3, n + 1):
+            a, b = b, a + b
+        return b
 
-    steps = 0
-    while n != 1:
-        if n % 2 == 0:
-            n = n // 2
-        else:
-            n = 3 * n + 1
-        steps += 1
 
-    return steps
+term = fib(2)
+print(term)
 
-# Test the function with different values for n
-n_values = [1000, 3, 27, 7]  # Add more values as needed
-
-for n in n_values:
-    steps = hailstone(n)
-    print(f"Steps to reach 1 for n = {n}: {steps}")
 
 
