bc9aa8f333a6afb5fc2224d5a70053b43c12dab3
Spencer-Daniel5650 <danspencer5650@gmail.com>
2023-10-25 17:30:48 -0400

    New

--- test_fib.py
+++ test_fib.py
@@ -3,18 +3,22 @@
 # Date: 10/23/2023
 # Description: Creates a function named fib that takes positive integer and returns the numerical location it resides in the fibonacci sequence.
 
-
-
 def fib(n):
     if n <= 0:
-        return 0
-    elif n == 1 or n == 2:
-        return 1
-    else:
-        fib_prev = 1
-        fib_current = 1
-        for _ in range(3, n + 1):
-            fib_next = fib_prev + fib_current
-            fib_prev, fib_current = fib_current, fib_next
-        return fib_current
+        return None  # Handle invalid input
 
+    if n == 1 or n == 2:
+        return 1  # Base case for the first two Fibonacci numbers
+
+    fib_prev = 1  # Initialize the previous Fibonacci number to 1
+    fib_curr = 1  # Initialize the current Fibonacci number to 1
+
+    for i in range(3, n + 1):
+        fib_next = fib_prev + fib_curr  # Calculate the next Fibonacci number
+        fib_prev, fib_curr = fib_curr, fib_next  # Update previous and current Fibonacci numbers
+
+    return fib_curr  # Return the Fibonacci number at position n
+
+# Example usage:
+term = fib(10)
+print(term)  # Output will be 55
