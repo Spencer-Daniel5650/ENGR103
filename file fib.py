60bbe09062b852468d431177a7c91821437a1a2e
Spencer-Daniel5650 <danspencer5650@gmail.com>
2023-10-25 18:36:08 -0400

    New

--- file fib.py
+++ file fib.py
@@ -3,28 +3,25 @@
 # Date: 10/23/2023
 # Description: Creates a function named fib that takes positive integer and returns the numerical location it resides in the fibonacci sequence.
 
-import unittest
-
-def fib(n):
-    if n <= 0:
+def hailstone(n):
+    if n == 1:
         return 0
-    elif n == 1 or n == 2:
-        return 1
-    else:
-        fib_prev = 1
-        fib_current = 1
-        for _ in range(3, n + 1):
-            fib_next = fib_prev + fib_current
-            fib_prev, fib_current = fib_current, fib_next
-        return fib_current
 
-class TestFib(unittest.TestCase):
-    def test_fibonacci(self):
-        self.assertEqual(fib(1), 1)
-        self.assertEqual(fib(3), 2)
-        self.assertEqual(fib(10), 55)
+    steps = 0
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+        steps += 1
 
-if __name__ == "__main__":
-    unittest.main()
+    return steps
+
+# Test the function with different values for n
+n_values = [1000, 3, 27, 7]  # Add more values as needed
+
+for n in n_values:
+    steps = hailstone(n)
+    print(f"Steps to reach 1 for n = {n}: {steps}")
 
 
