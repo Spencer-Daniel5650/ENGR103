fbe26bd6697edd0f3c395219d1fdfc839c026b8c
Spencer-Daniel5650 <danspencer5650@gmail.com>
2023-10-25 19:54:51 -0400

    Merge remote-tracking branch 'origin/main'

--- fib.py
+++ fib.py
@@ -3,17 +3,19 @@
 # Date: 10/23/2023
 # Description: Creates a function named fib that takes positive integer and returns the numerical location it resides in the fibonacci sequence.
 
-def test_fib(n):
+def fib(n):
+    if n <= 0:
+        return 0
+
     fib_prev = 1
     fib_current = 1
 
-    if n == 1 or n == 2:
-        return 1
-    else:
-        for _ in range(3, n + 1):
-            fib_next = fib_prev + fib_current
-            fib_prev, fib_current = fib_current, fib_next
-        return fib_current
+    for _ in range(3, n + 1):
+        fib_next = fib_prev + fib_current
+        fib_prev, fib_current = fib_current, fib_next
+
+    return fib_current
+
 
 
 
