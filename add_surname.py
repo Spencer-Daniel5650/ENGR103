6711bbc2c274c43d98aa531713f3ccac68cebe98
Spencer-Daniel5650 <danspencer5650@gmail.com>
2023-11-08 14:29:46 -0800

    New

--- add_surname.py
+++ add_surname.py
@@ -3,11 +3,8 @@
 # Date: 11/06/2023
 # Description: The add_surname function creates a new list of names by appending the surname "Kardashian" to each first name from the input list that starts with the letter "K".
 
-
-
-
-def add_surname(names):
+def add_surname(first_names):
     """
-    Return a list of names that start with 'K' with 'Kardashian' as the surname.
+    Return a list of names that start with 'K' with 'Kardashian' added as the surname.
     """
-    return [name + " " for name in names if name.startswith("
+    return [f"{name} Kardashian" for name in first_names if name.startswith('K')]
