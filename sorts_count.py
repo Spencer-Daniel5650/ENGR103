3bb160148ae8e9d72bf38e21431fb079e6cdf762
Spencer-Daniel5650 <danspencer5650@gmail.com>
2024-01-31 18:22:47 -0800

    Merge remote-tracking branch 'origin/main'

--- sorts_count.py
+++ sorts_count.py
@@ -4,30 +4,35 @@
 # Description: unction sorts a list using the bubble
 # sort algorithm and returns the number of element comparisons and swaps made
 
-def insertion_count(a_list):
-    """
-    Perform insertion sort on a_list and return a tuple (comparisons, exchanges)
-    where comparisons is the number of comparisons between elements and
-    exchanges is the number of times elements are inserted in a different position.
-    """
+def bubble_count(lst):
+    n = len(lst)
     comparisons = 0
     exchanges = 0
-    n = len(a_list)
+
+    for i in range(n):
+        for j in range(0, n-i-1):
+            comparisons += 1
+            if lst[j] > lst[j+1]:
+                lst[j], lst[j+1] = lst[j+1], lst[j]
+                exchanges += 1
+
+    return comparisons, exchanges
+
+def insertion_count(lst):
+    n = len(lst)
+    comparisons = 0
+    exchanges = 0
 
     for i in range(1, n):
-        key = a_list[i]
-        j = i - 1
-
-        # Perform comparisons and shift elements of a_list[0..i-1]
-        while j >= 0:
-            if a_list[j] > key:
-                comparisons += 1
-                a_list[j + 1] = a_list[j]
-                exchanges += 1
-                j -= 1
-            else:
-                comparisons += 1
-                break
-        a_list[j + 1] = key
+        key = lst[i]
+        j = i-1
+        while j >= 0 and key < lst[j]:
+            comparisons += 1
+            lst[j+1] = lst[j]
+            j -= 1
+            exchanges += 1
+        if j >= 0:
+            comparisons += 1  # For the last comparison where key >= lst[j]
+        lst[j+1] = key
 
     return comparisons, exchanges
